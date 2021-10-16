34dffb67861b2e7482b77860e8950e0d0de24d5f
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-10-16 20:21:54 +0400

    I try Dom js

--- Javascript/Begin to javascript/app.js
+++ Javascript/Begin to javascript/app.js
@@ -98,3 +98,84 @@ var test2 = frontend.indexOf("Html")
 document.write(test2 + "<br>")
 let fullstack = frontend.concat(backend);
 document.write(fullstack)
+document.write("<hr>")
+document.write(Math.ceil(0.00055))
+document.write("<hr>")
+document.write(Math.max(12, 100))
+
+document.write("<hr>")
+
+let command1 = prompt("Write a number1")
+let command2 = prompt("Write a number2")
+
+document.write(command1 + "<br>")
+document.write(command2)
+
+document.write("<hr>")
+
+let nmr = 0;
+nmr++;
+alert(nmr)
+document.write("<hr>");
+
+let age = prompt("How old are you?")
+if (18 > age) {
+    document.write("Sen 18 yasdan balacasan ve bu sehifede qalmamalisan")
+} else {
+    document.write("Sehufeye xos gelmisen");
+}
+
+
+let point = prompt("Nece bal toplamisan?");
+
+if (point >= 55 && point <= 75) {
+    document.write("Senin qiymetin 3 dur");
+} else if (point >= 75 && point <= 85) {
+    document.write("Senin qiymetin 4 dur");
+} else if (point >= 85 && point <= 100) {
+    document.write("Senin qiymetin 5 dir");
+} else {
+    document.write("Cix cole");
+}
+document.write("<hr>")
+
+let i = 0;
+let p = 0;
+while (i < 10) {
+    p += i;
+    i++;
+}
+document.write(p)
+
+document.write("<hr>")
+
+for (let i = 1; i < 100; i++) {
+    if (i % 5 == 0) {
+        console.log(i)
+    }
+
+}
+
+let elements = ["apple", "banana", "orange", "leamon", "grape"];
+elements.push("Muhammad")
+document.write(elements)
+
+let universitys = ['AzTu', 'ADNSU', 'BMU', 'ADPU'];
+let slice = universitys.slice(0, 3).join("-")
+document.write(slice)
+
+document.getElementById("btn").onclick = function() {
+    let top = document.getElementById("container-top").innerHTML;
+    let bottom = document.getElementById("container-bottom").innerHTML;
+    document.getElementById("container-top").innerHTML = bottom;
+    document.getElementById("container-bottom").innerHTML = top;
+}
+document.getElementById("btn-change").onclick = function() {
+    document.getElementsByClassName("text1")[0].innerHTML = "Mehemmed";
+    document.getElementsByClassName("text2")[0].innerHTML = "Aliyev";
+
+}
+document.getElementById("color").onclick = function() {
+    document.getElementById("container-top").style.color = "red";
+    document.getElementById("container-bottom").style.backgroundColor = "blue"
+}
