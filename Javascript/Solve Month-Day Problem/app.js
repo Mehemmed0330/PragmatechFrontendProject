685193815f3ead9ec6e25d744bf92522f9abe562
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-10-25 21:28:36 +0400

    Month names

--- Javascript/Solve Month-Day Problem/app.js
+++ Javascript/Solve Month-Day Problem/app.js
@@ -1,5 +1,15 @@
-function Month(day) {
-    let month = new Date('2021, 05, 25')
-    console.log(month);
+let myStringArray = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December", "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+
+let arrayLength = myStringArray.length;
+
+function Month() {
+    for (let i = 9; i < 16; i++) {
+        console.log(myStringArray[i]);
+        //Do something
+    }
 }
 Month();
