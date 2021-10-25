a786f678f3341b54a70c9a5818e4d5712dbfeca1
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-10-26 01:03:25 +0400

    Month-Day

--- Javascript/Solve Month-Day Problem/app.js
+++ Javascript/Solve Month-Day Problem/app.js
@@ -1,15 +1,9 @@
-let myStringArray = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December", "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-];
+const start = new Date("03/25/2020");
+const end = new Date("07/25/2020");
 
-
-let arrayLength = myStringArray.length;
-
-function Month() {
-    for (let i = 9; i < 16; i++) {
-        console.log(myStringArray[i]);
-        //Do something
-    }
+let loop = new Date(start);
+while (loop <= end) {
+    console.log(loop);
+    let newDate = loop.setDate(loop.getDate() + 1);
+    loop = new Date(newDate);
 }
-Month();
