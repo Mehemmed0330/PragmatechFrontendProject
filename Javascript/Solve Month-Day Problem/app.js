ac8b8b92113db57b17fa9883fe23f5e934f07a47
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-10-27 02:00:19 +0400

    Month-Date

--- Javascript/Solve Month-Day Problem/app.js
+++ Javascript/Solve Month-Day Problem/app.js
@@ -1,9 +1,25 @@
-const start = new Date("03/25/2020");
-const end = new Date("07/25/2020");
+// const start = new Date("03/25/2020");
+// const end = new Date("07/25/2020");
 
-let loop = new Date(start);
-while (loop <= end) {
-    console.log(loop);
-    let newDate = loop.setDate(loop.getDate() + 1);
-    loop = new Date(newDate);
+// let loop = new Date(start);
+// while (loop <= end) {
+//     console.log(loop);
+//     let newDate = loop.setDate(loop.getDate() + 1);
+//     if (Date.getDate === Date.getDate) {
+//         loop = new Date(newDate);
+//     }
+// }
+
+function date() {
+    const start = new Date("03/25/2020");
+    const end = new Date("07/25/2020");
+    let loop = new Date(start);
+    while (loop <= end) {
+        console.log(loop);
+        let newDate = loop.setDate(loop.getDate() + 1);
+        if (loop.getDate == loop.getDate) {
+            loop = new Date(newDate);
+        }
+    }
 }
+date();
