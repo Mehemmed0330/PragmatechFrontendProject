5de87840eee5bd483fe674df7c1c99e3043d726c
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-10-29 15:19:56 +0400

    Ready modal in project with javascript

---
+++ Javascript/Sticky Notes/app.js
@@ -0,0 +1,8 @@
+function AddNote() {
+    document.getElementById("modal").style.display = "block";
+    console.log("hi");
+}
+
+function Close() {
+    document.getElementById("modal").style.display = "none";
+}
