126d3e9555b152971e850b40baabd4a312f75a99
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-11-01 23:03:27 +0400

    Add word

--- Javascript/Sticky Notes/app.js
+++ Javascript/Sticky Notes/app.js
@@ -7,7 +7,28 @@ function Close() {
     document.getElementById("modal").style.display = "none";
 }
 
-function sub() {
-    let textaa = document.getElementById("CommendAdd").value;
-    document.getElementById("text").innerText = textaa;
-}
+// function sub() {
+//     let textaa = document.getElementById("CommendAdd").value;
+//     document.getElementById("text").innerText = textaa;
+// }
+
+// function sub() {
+
+//     var li = document.createElement("li");
+//     var inputValue = document.getElementById("CommendAdd").value;
+//     var t = document.createTextNode(inputValue);
+//     li.appendChild(t);
+//     document.getElementById("myUL").appendChild(li);
+//     document.getElementById("CommendAdd").value;
+// }
+
+let formm = document.querySelector("#Form");
+formm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("CommendAdd").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    document.getElementById("myUL").appendChild(li);
+    document.getElementById("CommendAdd").value;
+})
