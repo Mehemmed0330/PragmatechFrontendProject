921f4fa986d5fe2a5a2d97d16f929d340f137d69
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-11-12 23:21:05 +0400

    Quiz app ...

--- ChallangeWeek03/js/app.js
+++ ChallangeWeek03/js/app.js
@@ -3,3 +3,34 @@
 //     document.querySelector("#btn").style.border = "2px solid blue";
 
 // })
+
+quiz = [{
+    'question': 'What is your name',
+    'answser': [
+        'Muhammad',
+        'Farid',
+        'Jane',
+        'Moly',
+        'Unknown'
+    ]
+}]
+
+document.querySelector(".buttons").innerHTML = `
+                        <button class="btn" id="btn1" onclick="chechAnswer(this)">${quiz[0]['answer'][0]}</button>
+                        <button class="btn" id="btn2" onclick="chechAnswer(this)">${quiz[0]['answer'][1]}</button>
+                        <button class="btn" id="btn3" onclick="chechAnswer(this)">${quiz[0]['answer'][2]}</button>
+                        <button class="btn" id="btn4" onclick="chechAnswer(this)">${quiz[0]['answer'][3]}</button>
+                        <button class="btn" id="btn5" onclick="chechAnswer(this)">${quiz[0]['answer'][4]}</button>
+                        <p>Heloo</p>
+
+`
+
+function checkAnswer(el) {
+    if (el.innerHTML == 'Muhammad') {
+        el.className = 'backgorund-color:green;'
+    }
+}
+
+document.querySelector(".app").innerHTML = `
+<h1>Hello</h1>
+`
