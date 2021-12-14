df27e0e6b0c4230a4e7a474bbc21bfd49c6bd07d
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-12-15 01:13:28 +0400

    OOP

---
+++ Javascript/Object Orianted Programming/app.js
@@ -0,0 +1,47 @@
+// let person = {
+//     name: "Muhammad",
+//     "2313kaksds": "Aliev",
+//     number: true,
+
+// }
+// console.log(person.number)
+
+// let person = {};
+// person["name"] = true
+// console.log(person["name"])
+
+// let fruite = prompt("What is your favorite fruite?")
+// let fruites = {
+//     fruite: fruite
+// }
+// console.log(fruites.fruite)
+
+// function person(name, age) {
+//     return {
+//         name: name,
+//         age: age
+//     }
+
+// }
+// let user = person("Muhammad", 18)
+// console.log(user.name)
+
+// let user = {
+//     name: "Muhammad",
+//     age: 1
+// }
+// for (let key in user) {
+//     console.log(age[key])
+// }
+
+// let user = {
+//     name: "Muhammad"
+// }
+// let person = user
+// person.name = "Farid"
+// console.log(person.name)
+
+let person = {}
+person.age = 18;
+
+console.log(person.age)
