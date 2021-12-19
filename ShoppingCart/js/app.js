8da130840dee3c0375096ae295b365769cdd7ab6
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-12-19 17:21:48 +0400

    tranfer from json data to js

---
+++ ShoppingCart/js/app.js
@@ -0,0 +1,32 @@
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let data = JSON.parse(this.responseText);
+        for (let i = 0; i < data.length; i++) {
+            document.querySelector('.carts').innerHTML = `
+            
+            <div class="cart">
+           <div class="cart__img">
+           <img src="../image/itemimg1.png" alt="">
+             </div>
+            <div class="cart__about">
+            <div class="cart__about--value">
+               <h1><i class="fas fa-euro-sign"><span class="value">${data[i++].price}</span></i></h1>
+           </div>
+           <div class="cart__about--time">
+               <h1><i class="fas fa-plus"></i><span class="pro-name">${data[i++].name}</span></h1>
+           </div>
+          </div>
+          </div>
+          
+
+          
+          
+           `
+
+        }
+
+    }
+};
+xhttp.open("GET", "data/product.json", true);
+xhttp.send();
