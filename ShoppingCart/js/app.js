6c07cbb05f2d9b31c650475221e7e5ce36316578
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-12-20 21:28:29 +0400

    from json to html

--- ShoppingCart/js/app.js
+++ ShoppingCart/js/app.js
@@ -32,9 +32,22 @@ fetch('data/product.json')
     .then(data => {
         // data.preventDefault();
         for (let i = 0; i < data.length; i++) {
-
-            cart += `<span class="pro-value">${data[i++].price}</span>`
-            cart += `<span class="pro-name">${data[i++].name}</span>`
+            cart += `
+            <div class="cart">
+            <div class="cart__img">
+            <img src="image/itemimg1.png" alt="">
+        </div>
+        <div class="cart__about">
+            <div class="cart__about--value">
+                <h1><i class="fas fa-euro-sign"></i><span class="pro-value">${data[i++].price}</span></h1>
+            </div>
+            <div class="cart__about--time">
+                <h1><i class="fas fa-plus"></i><span class="pro-name">${data[i++].name}</span></h1>
+            </div>
+        </div>
+        </div>
+            `
+                // cart += `<span class="pro-name">${data[i++].name}</span>`
         }
         document.querySelector('.carts').innerHTML = cart;
         console.log(data);
