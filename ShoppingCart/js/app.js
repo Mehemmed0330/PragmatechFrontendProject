056eca34a7df3694536f810bb7c8c43677473cb5
Muhammad <mehemmedeliyevn0330@gmail.com>
2021-12-23 01:54:10 +0400

    data transfer in json

--- ShoppingCart/js/app.js
+++ ShoppingCart/js/app.js
@@ -1,54 +1,32 @@
-// let xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         let data = JSON.parse(this.responseText);
-//         for (let i = 0; i < data.length; i++) {
-//             document.querySelector('.carts').innerHTML = `
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let data = JSON.parse(this.responseText);
+        let carts = document.querySelector('.carts');
+        data.forEach(element => {
 
-//         <div class="cart">
-//            <div class="cart__img">
-//            <img src="../image/itemimg1.png" alt="">
-//              </div>
-//             <div class="cart__about">
-//             <div class="cart__about--value">
-//                <h1><i class="fas fa-euro-sign"><span class="value">${data[i++].price}</span></i></h1>
-//            </div>
-//            <div class="cart__about--time">
-//                <h1><i class="fas fa-plus"></i><span class="pro-name">${data[i++].name}</span></h1>
-//            </div>
-//           </div>
-//         </div>
-//            `
-//         }
+            carts.innerHTML += viewData(element)
+        });
 
-//     }
-// };
-// xhttp.open("GET", "data/product.json", true);
-// xhttp.send();
+    }
+};
+xhttp.open("GET", "data/product.json", true);
+xhttp.send();
 
-// let cart = ""
-// fetch('data/product.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         // data.preventDefault();
-//         for (let i = 0; i < data.length; i++) {
-//             cart += `
-//             <div class="cart">
-//             <div class="cart__img">
-//             <img src="image/itemimg1.png" alt="">
-//         </div>
-//         <div class="cart__about">
-//             <div class="cart__about--value">
-//                 <h1><i class="fas fa-euro-sign"></i><span class="pro-value">${data[i++].price}</span></h1>
-//             </div>
-//             <div class="cart__about--time">
-//                 <h1><i class="fas fa-plus"></i><span class="pro-name">${data[i++].name}</span></h1>
-//             </div>
-//         </div>
-//         </div>
-//             `
-//                 // cart += `<span class="pro-name">${data[i++].name}</span>`
-//         }
-//         document.querySelector('.carts').innerHTML = cart;
-//         console.log(data);
-//     })
+function viewData(product) {
+    return `
+    <div class="cart" data = ${product.id}>
+           <div class="cart__img">
+           <img src="${product.img}" alt="">
+             </div>
+            <div class="cart__about">
+            <div class="cart__about--value">
+               <h1><i class="fas fa-euro-sign"><span class="value">${product.price}</span></i></h1>
+           </div>
+           <div class="cart__about--time">
+               <h1><i class="fas fa-plus"></i><span class="pro-name">${product.name}</span></h1>
+           </div>
+          </div>
+        </div>
+    `
+}
