84c625f4b3fde26913af0ba096f48b2e1763cb95
Muhammad <mehemmedeliyevn0330@gmail.com>
2022-01-15 01:33:47 +0400

    Login form with useRef

--- React/Login Form/login/src/components/Login.jsx
+++ React/Login Form/login/src/components/Login.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import "./Login.css"
 import { useState,useRef } from 'react'
 
 export default function Login() {
     const [ , setRender] = useState({});
     const [answer, setanswer] = useState();
-    const nameRef = useRef();
-    const passwordRef = useRef();
+    const [color, setcolor] = useState();
+    const nameRef = useRef(false);
+    const passwordRef = useRef(false);
+    
+  
 
   function focus(){   
       setRender({});
       if (nameRef.current.value === "Mehemmed" && passwordRef.current.value === "2003" ) {
           setanswer("Xos gelmsisiz")
-      }else if(nameRef.current.value === "Mehemmed" || passwordRef.current.value === " "){
+      }else if(nameRef.current.value === "Mehemmed" && passwordRef.current.value !== "2003"){
         setanswer("Ad duzdur")
-      }else if(nameRef.current.value === " " || passwordRef.current.value === "2003"){
+      }else if(nameRef.current.value !== "Mehemmed" && passwordRef.current.value === "2003"){
         setanswer("Kod duzdur")
       }else{
           setanswer("Cix burdan")
@@ -22,14 +26,14 @@ export default function Login() {
   }
 
     return (
-        <>
-        <div className="contaienr">
-        <input ref={nameRef} type="text" />
-        <input ref={passwordRef} type="password" />
-        <button onClick={focus}>Click</button>
-        <div className="class">{answer}</div>
+        
+        <div className="container">
+            <input ref={nameRef} type="text" placeholder='name' /> <br></br>
+            <input ref={passwordRef} type="password" placeholder='password' /> <br></br>
+            <button onClick={focus}>Click</button>
+            <div className="class">{answer}</div>
         </div>            
-        </>
+        
         
     )
 }
