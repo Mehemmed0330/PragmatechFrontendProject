ed10104dc64f984507d86562cf26190203bec911
Muhammad <mehemmedeliyevn0330@gmail.com>
2022-01-22 20:05:28 +0400

    Page

--- React/Project/simple-project/src/components/Navbar.jsx
+++ React/Project/simple-project/src/components/Navbar.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import { Link ,NavLink } from 'react-router-dom';
 import "./Navbar.css"
+import { useState } from 'react';
+
 
 const Navbar = () => {
+    const [link, setLink] = useState("hello");
+
+    const click = ()=>{
+        setLink(link)
+    }
     return (
         <div>
             <div className="container">
                 <div className="navbar">
                     <Link style={{ textDecoration: 'none', color:"red " , padding:"12px 8px", backgroundColor:"black"}} to={"/"}>Home</Link>
-                    <Link style={{textDecoration: 'none', color:"red " , padding:"12px 8px", backgroundColor:"black"}} to ={"/contact"}>Contact</Link>
+                    <Link onClick={click} style={{textDecoration: 'none', color:"red " , padding:"12px 8px", backgroundColor:"black"}} to ={"/contact"}>Contact</Link>
                     <NavLink
                         style={({ isActive }) =>
                         isActive ? {
                             color:"red"
                             }  : undefined
                         }
-                        to="/team">Team</NavLink>          
-
-          
+                        to="/team">Team
+                    </NavLink>  
+                      
+                       {/* { <div style={{
+                            backgroundColor:"green",
+                            width:"15px",
+                            height:"15px"
+                        }}>{setLink(link)}</div>
+                        } */}
+                        {link}
+                        
                  </div>
             </div>
         </div>
