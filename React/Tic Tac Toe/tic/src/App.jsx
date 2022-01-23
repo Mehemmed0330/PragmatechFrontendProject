1244108502872639a5aee18a27a50bc9c2888609
Muhammad <mehemmedeliyevn0330@gmail.com>
2022-01-23 18:11:03 +0400

    Tic Tac Toe

--- React/Tic Tac Toe/tic/src/App.jsx
+++ React/Tic Tac Toe/tic/src/App.jsx
@@ -15,14 +15,22 @@ function App() {
   ]);
  
   const [fell, setFell] = useState("X")
-
+  const [winner,setWinner] = useState(null);
+  const [isFinished,setIsFinished] = useState(false);
  
   const click = (id) => {
     setValue(pre=>{
       let copyPre = [...pre];
       let index = copyPre.findIndex(item=>item.id === id);
       copyPre[index].element = fell;
-      
+      if((copyPre[0].element === "X" && copyPre[1].element === "X" && copyPre[2].element === "X") || (copyPre[3].element === "X" && copyPre[4].element === "X" && copyPre[5].element === "X") || (copyPre[6].element === "X" && copyPre[7].element === "X" && copyPre[8].element === "X") || (copyPre[0].element === "X" && copyPre[3].element === "X" && copyPre[6].element === "X")|| (copyPre[1].element === "X" && copyPre[4].element === "X" && copyPre[7].element === "X") || (copyPre[2].element === "X" && copyPre[5].element === "X" && copyPre[8].element === "X")|| (copyPre[0].element === "X" && copyPre[4].element === "X" && copyPre[8].element === "X")|| (copyPre[2].element === "X" && copyPre[4].element === "X" && copyPre[6].element === "X")) {
+        setWinner("X");
+        setIsFinished(true);
+      }
+      if((copyPre[0].element === "O" && copyPre[1].element === "O" && copyPre[2].element === "O") || (copyPre[3].element === "O" && copyPre[4].element === "O" && copyPre[5].element === "O") || (copyPre[6].element === "O" && copyPre[7].element === "O" && copyPre[8].element === "O") || (copyPre[0].element === "O" && copyPre[3].element === "O" && copyPre[6].element === "O")|| (copyPre[1].element === "O" && copyPre[4].element === "O" && copyPre[7].element === "O") || (copyPre[2].element === "O" && copyPre[5].element === "O" && copyPre[8].element === "O")|| (copyPre[0].element === "O" && copyPre[4].element === "O" && copyPre[8].element === "O")|| (copyPre[2].element === "O" && copyPre[4].element === "O" && copyPre[6].element === "O")) {
+        setWinner("O");
+        setIsFinished(true);
+      }
       
       return copyPre
     });
@@ -31,11 +39,11 @@ function App() {
 
   return (
     <div className="App">
-      
+      {isFinished && <span>Qalib {winner}-dir</span>}
       <div style={{display:"flex",flexWrap:"wrap"}}>
        {value.map((item,index)=>{
         return( <div className="container" key={item.id}>
-          <div  className="box" onClick={()=>click(item.id)}>{item.element}</div>
+          <div style={isFinished ? {pointerEvents:"none"}:{}} className="box" onClick={()=>click(item.id)}>{item.element}</div>
         </div>
         )
        })}
