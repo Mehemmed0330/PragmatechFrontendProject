7d319451749f7fdaeb2dfb1826c0a6cd9e7523e7
Muhammad <mehemmedeliyevn0330@gmail.com>
2022-02-02 09:20:08 +0400

    Ready register

--- React/Netflix-clone/netflix-clone/src/components/Finish.jsx
+++ React/Netflix-clone/netflix-clone/src/components/Finish.jsx
@@ -3,18 +3,21 @@ import React from 'react';
 // import { useParams } from 'react-router-dom';
 // import { Name } from '../components/Stepthree';
 import { useLocation } from 'react-router-dom';
-
-
-
+import { useNavigate } from 'react-router-dom';
+import "../css/Finish.css"
 const Finish = () => {
 //  const {name} = useContext(Name)
-const {name,plan}=useLocation().state
+const {name,plan}=useLocation().state;
+const navigate = useNavigate();
     return (
-             <div>
-                {name}
-                {plan}
+             <div style={{color:"white"}}>
+               <div className="success">
+                   <h2>Congratulations, <span style={{color:"red"}}>{name}!</span></h2>
+                   <h1>You've registered for the <span style={{color:"red"}}>{plan}</span> plan,
+                    but don't worry, we wont charge you until your trials up.</h1>
+                    <button className='brows' onClick={()=>navigate("/")}>START BROWSING</button>
+               </div>
             </div>
-       
     );
 }
 
--- React/Netflix-clone/netflix-clone/src/components/Stepthree.jsx
+++ React/Netflix-clone/netflix-clone/src/components/Stepthree.jsx
@@ -1,8 +1,7 @@
-import React, { createContext } from 'react';
+import React from 'react';
 import "../css/Stepthree.css"
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-export const Name = createContext();
 const Stepthree = () => {
     const {plan} = useLocation()?.state
     const [accesslast, setaccesslast] = useState("#e50914 ");
@@ -18,20 +17,15 @@ const Stepthree = () => {
                 <h1>Sign up to start your free month</h1>
                 <p>Create your Account</p>
             </div>
-            
-            <Name.Provider value={[name]}>
-                {/* <Finish/> */}
             <form action="">
                 <input type="text" placeholder='FirstName' onChange={(e)=>setname(e.target.value)} /> 
-                <input type="text" placeholder='LastName'  /> 
-                <input type="text" placeholder='Email'  /> 
+                <input type="text" placeholder='LastName' /> 
+                <input type="text" placeholder='Email' /> 
                 <input type="password" placeholder='Password' /> 
                 <button onMouseEnter={()=>changelast("#f6121d")} onMouseOut={()=>changelast("#e50914")} 
                 onClick={()=>navigate("/finish" , {state:{name:name,plan:plan}})
                 } style={{backgroundColor:`${accesslast}`,padding:"14px 150px",transition:"all 0.5s ease",width:"401.73px",outline:"none",border:"none",color:"white",fontSize:"18px",cursor:"pointer"}}>CONFIRM</button>
             </form>
-           </Name.Provider> 
-            
         </div>
     );
 }
