6b748f640cbd5f1dd2a25f67de68a39272a260bd
Muhammad <mehemmedeliyevn0330@gmail.com>
2022-02-24 23:04:02 +0400

    Netflix

--- React/Netflix/full-netflix/src/components/Success.jsx
+++ React/Netflix/full-netflix/src/components/Success.jsx
@@ -10,7 +10,7 @@ const Success = () => {
                 <h1 style = {{fontSize:"25px"}}>You've registered for the <span style={{color:"red"}}>{plan}</span> plan, <br/>
                     but don't worry, we wont charge you until your trials up.
                 </h1>
-                <button onClick = {() => navigate("")}
+                <button onClick = {() => navigate("/")}
                  style = {{width:"100%" ,textAlign:"center",padding:"10px" , backgroundColor :"#e50914" , outline:"none", color:"white", fontSize:"18px" , border:"none" ,cursor:"pointer"}}>START BROWSING</button>
             </div>
         </div>
