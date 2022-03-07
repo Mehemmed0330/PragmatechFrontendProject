30344046e7cf179aa0dde369915d597d507923bd
Muhammad <mehemmedeliyevn0330@gmail.com>
2022-03-07 23:55:57 +0400

    Redux

---
+++ React/Reduxx/redux-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+    </div>
+  )
+}
+
+export default App
